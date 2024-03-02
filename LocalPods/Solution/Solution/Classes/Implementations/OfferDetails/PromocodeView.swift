fab827d8c0fc9675e3d4a4be4bc4e8b55ed6e47e
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2024-03-02 15:34:21 +0300

    Task-9

--- LocalPods/Solution/Solution/Classes/Implementations/OfferDetails/PromocodeView.swift
+++ LocalPods/Solution/Solution/Classes/Implementations/OfferDetails/PromocodeView.swift
@@ -15,7 +15,24 @@ final class PromocodeView: UIView, IPromocodeView {
     
     // UI
     
-    // --TODO--
+    private let title = {
+        let view = UILabel()
+        view.font = .systemFont(ofSize: 14, weight: .regular)
+        view.text = "Промокод"
+        return view
+    }()
+    private let promocode = {
+        let view = UILabel()
+        view.font = .systemFont(ofSize: 14, weight: .regular)
+        return view
+    }()
+    private let image = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.image = UIImage(systemName: "rectangle.portrait.on.rectangle.portrait")
+        view.tintColor = .black
+        return view
+    }()
     
     // init
     
@@ -23,6 +40,7 @@ final class PromocodeView: UIView, IPromocodeView {
         super.init(frame: .zero)
         
         isHidden = true
+        setupUI()
     }
     
     @available(*, unavailable)
@@ -34,5 +52,43 @@ final class PromocodeView: UIView, IPromocodeView {
     
     func configure(code: String) {
         isHidden = false
+        promocode.text = code
+    }
+    
+}
+
+private extension PromocodeView {
+    func setupUI() {
+        addSubview(title)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(promocode)
+        promocode.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(image)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            title.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            title.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -11),
+            title.topAnchor.constraint(equalTo: topAnchor, constant: 11),
+            
+            image.heightAnchor.constraint(equalToConstant: 17),
+            image.widthAnchor.constraint(equalToConstant: 17),
+            image.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -11),
+            image.topAnchor.constraint(equalTo: topAnchor, constant: 11),
+            image.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            
+            promocode.trailingAnchor.constraint(equalTo: image.leadingAnchor, constant: -8),
+            promocode.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -11),
+            promocode.topAnchor.constraint(equalTo: topAnchor, constant: 11),
+        ])
+        
+        setupSelf()
+    }
+    
+    func setupSelf() {
+        backgroundColor = #colorLiteral(red: 0.9215685725, green: 0.9215685725, blue: 0.9215685725, alpha: 1)
+        layer.cornerRadius = 12
+        layer.masksToBounds = true
     }
 }
